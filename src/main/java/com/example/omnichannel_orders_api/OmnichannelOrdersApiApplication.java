e29ad877679b5dcc9c72e53e08edfac22d715b08
Roberto Neto <rosone96@gmail.com>
2026-06-04 22:10:36 -0300

    Initial project configuration

---
+++ src/main/java/com/example/omnichannel_orders_api/OmnichannelOrdersApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.omnichannel_orders_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnichannelOrdersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnichannelOrdersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/omnichannel_orders_api/OmnichannelOrdersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.omnichannel_orders_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnichannelOrdersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
